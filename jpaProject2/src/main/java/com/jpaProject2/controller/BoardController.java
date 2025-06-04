c9a5ee318ab0a108bef91d9f808f8128f9082857
eunjin900808 <eunjin900808@naver.com>
2025-06-04 17:50:24 +0900

    Add files via upload

--- jpaProject2/src/main/java/com/jpaProject2/controller/BoardController.java
+++ jpaProject2/src/main/java/com/jpaProject2/controller/BoardController.java
@@ -28,35 +28,41 @@ public class BoardController {
 		return model;
 	}
 	
-	@PostMapping
+	@PostMapping ///save 같은 경로에 대한 POST 요청을 처리
 	public String save(BoardDto dto) {
-		String msg="ok";
-		BoardDto dto1 = boardService.save(dto);
+		String msg="ok"; // 기본적으로 결과 메시지를 "ok"로 설정
+		BoardDto dto1 = boardService.save(dto); //저장된 결과(예: DB에 저장된 객체)가 dto1에 반환됨
 		if(dto1 == null) {
-			msg = "fail";
+			msg = "fail"; //만약 저장 결과가 null 이라면(즉, 저장 실패), 메시지를 "fail"로 바꿈
 		}
-		return msg;
+		return msg; // 최종적으로 저장 성공이면 "ok", 실패면 "fail" 문자열을 클라이언트에 반환
 	}
 	
 	@GetMapping
-	public ModelAndView list(@RequestParam(defaultValue="1") int indexpage) { // 1페이지로 설정
+	public ModelAndView list(@RequestParam(defaultValue="1") int indexpage) { // indexpage 변수를 URL에서 받는다. (예: /board/list?indexpage=2 → indexpage = 2)
+		// @RequestParam(defaultValue="1") :::  값이 없으면 기본값 1로 처리
 		ModelAndView model = new ModelAndView();
 		
-		Long total = boardService.count();
+		Long total = boardService.count(); // 전체 게시글 수를 가져옴
+
 		//List<BoardDto> list = boardService.list();
-		
+		// 게시글을 페이지 단위로 조회
 		Page<BoardDto> page = boardService.list(indexpage-1,5); // indexpage는 1부터 시작 0이 셋팅되야하기때문에 -1 을 해준다
+			//indexpage - 1: 페이지 인덱스는 0부터 시작하므로, 사용자가 1페이지를 요청해도 내부에서는 0페이지로 처리
+			//5: 한 페이지당 5개의 게시글을 가져옴
 		
 		// 예) 현재 페이지 번호 2번인 경우 :
 		// int startPageRownum = 총데이터 개수 - (2-1)*5;
 		// 화면 출력 시작 번호 = (총 데이터 개수 - (현재페이지번호-1)*출력단위)
 		int startPageRownum = (int) (page.getTotalElements() - page.getNumber()*5);
+		// 게시판에서 번호를 거꾸로 매기는 경우(최신 글이 위에 오고 번호가 내려감) 사용하는 방식
 		
 		model.addObject("plist", page.getContent());
+		// 현재 페이지의 게시글 리스트(List<BoardDto>)를 모델에 담
 		//model.addObject("currentPage", page.getNumber());
 		model.addObject("startPageRownum", startPageRownum);
-		model.addObject("ptotal", page.getTotalElements());
-		model.addObject("ptotalPage", page.getTotalPages());
+		model.addObject("ptotal", page.getTotalElements()); // 전체 게시글 수
+		model.addObject("ptotalPage", page.getTotalPages()); // 전체 페이지 수
 		
 		model.setViewName("/board/nboardList");
 		return model;
--- jpaProject2/src/main/java/com/jpaProject2/service/BoardService.java
+++ jpaProject2/src/main/java/com/jpaProject2/service/BoardService.java
@@ -26,7 +26,8 @@ public class BoardService {
 	 */
 	//public List<BoardDto> list() {
 	public Page<BoardDto> list(int page,int size) { // 출력페이지 개수, 화면에 출력할페이지 개수
-		
+		// page : 조회할 페이지 번호 (0부터 시작)
+		// size : 한 페이지당 게시글 수
 		Pageable pageable = PageRequest.of(page, size);
 		return boardRepository.findAll(pageable);
 	}
@@ -35,6 +36,7 @@ public class BoardService {
 	 * 총 데이터 개수
 	 */
 	public Long count() {
-		return boardRepository.count();
+		return boardRepository.count(); // 테이블에 저장된 전체 게시글의 개수를 반환
+		// 반환 타입이 Long인 이유 : → 게시글 수가 많을 수 있으므로, int보다 더 큰 범위를 다룰 수 있도록 Long을 사용
 	}
 }
